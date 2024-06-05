71dcf404f4844567c97960a02f77d450363284d5
programmerHari18 <bogaramharihara@gmail.com>
2024-06-05 13:08:24 +0530

    End of Section 4

---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,15 @@
+
+
+using Domain;
+using AutoMapper;
+
+namespace Application.Core
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Activity,Activity>();
+        }
+    }
+}
